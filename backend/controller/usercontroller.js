b161ef883bb02851bb0287e70c7829e0e33a6699
nirali987 <niraliposiya83@gmail.com>
2024-02-23 08:44:30 +0530

    msg

---
+++ backend/controller/usercontroller.js
@@ -0,0 +1,19 @@
+var usermodel = require('../model/usermodel');
+
+exports.insert = async(req,res) => {
+    var data = await usermodel.create(req.body);
+
+    res.status(200).json({
+        status:'success',
+        data
+    })
+}
+
+exports.select_data = async(req,res) => {
+    var data = await usermodel.find();
+
+    res.status(200).json({
+        status:'success',
+        data
+    })
+}
